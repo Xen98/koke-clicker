7e8f5ad00b801d8d9301129f5f65357e4a051017
Xen <luisgpm98@gmail.com>
2022-03-24 02:22:23 -0600

    Cambios interfaz y sonido pop

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,16 +4,33 @@ const btnSkin = document.getElementById('cambiarSkin');
 const listaSkins = document.getElementById('listaSkins');
 const skins = document.getElementById('skins');
 const titulo = document.getElementById('titulo');
+const colorMode = document.getElementById('color-button');
+const iconColor = document.getElementById('icon-color');
+const body = document.body;
+
+const dark = true;
 
 let valorContador = 0;
 const audS4C = new Audio('assets/sounds/suaj4clicker.wav');
+const audPop = new Audio('assets/sounds/pop.mp3');
+
+audPop.volume = 0.3;
+audS4C.volume = 0.6;
 
 const refSkins = {
   skinKoke: 'assets/img/koke.png',
   skinJargua: 'assets/img/jargua.png',
   skinGomers: 'assets/img/gomers.png',
   skinChen: 'assets/img/chen.png'
-} 
+}
+
+const cambiarTema = () => {
+  body.classList.toggle('dark');
+  body.classList.toggle('light');
+
+  iconColor.classList.toggle('fa-moon');
+  iconColor.classList.toggle('fa-sun');
+}
 
 const initContador = () => {
   if (localStorage) {
@@ -28,6 +45,9 @@ const setContador = (valor) => {
 }
 
 const contadorKoke = () => {
+  audPop.pause();
+  audPop.currentTime = 0;
+  audPop.play();
   valorContador++;
   setContador(valorContador);
 }
@@ -38,10 +58,12 @@ const mostrarSkins = () => {
 
 const seleccionarSkin = (e) => {
   skinId = e.target.id;
-  if (skinId != 'skins') {
+  if (skinId != 'skins' && skinId) {
     koke.setAttribute('src', refSkins[skinId]);
   }
 }
+
+colorMode.addEventListener('click', cambiarTema);
 
 koke.addEventListener('click', contadorKoke);
 
@@ -50,6 +72,23 @@ btnSkin.addEventListener('click', mostrarSkins);
 skins.addEventListener('click', seleccionarSkin);
 
 titulo.addEventListener('click', () => audS4C.play())
+
+
+titulo.addEventListener('touchstart', () => {
+  titulo.classList.add('color-primary');
+});
+
+titulo.addEventListener('touchend', () => {
+  titulo.classList.remove('color-primary');
+});
+
+btnSkin.addEventListener('touchstart', () => {
+  btnSkin.classList.add('color-primary');
+});
+
+btnSkin.addEventListener('touchend', () => {
+  btnSkin.classList.remove('color-primary');
+});
 
 initContador();
 
